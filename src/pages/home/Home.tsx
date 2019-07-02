1fa6978f5f2b84821f6c02034d1de930e700bd05
Gaston Elordi <gelordi@aracargroup.com>
2019-07-02 13:13:11 -0300

    Home page - User logged profile

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,33 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Redirect, Switch } from "react-router-dom";
 import DefaultLayout from "../../components/layouts/DefaultLayout";
-import Dummy from "../../components/Dummy";
 import UserProfileContainer from "../User/UserProfileContainer";
 import ArtistsSearchContainer from "../Artist/Search/ArtistSearchContainer";
 import TracksSearchContainer from "../Track/Search/TrackSearchContainer";
 
-class Home extends React.Component<any, any> {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <DefaultLayout exact path="/" component={Dummy} />
-          <DefaultLayout exact path="/me" component={UserProfileContainer} />
-          <DefaultLayout
-            exact
-            path="/artists"
-            component={ArtistsSearchContainer}
-          />
-          <DefaultLayout
-            exact
-            path="/songs"
-            component={TracksSearchContainer}
-          />
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-    );
-  }
-}
+interface HomeProps {}
+
+const Home: React.SFC<HomeProps> = (props) => {
+  return (
+    <Router>
+      <Switch>
+        <DefaultLayout exact path="/" component={UserProfileContainer} />
+        <DefaultLayout
+          exact
+          path="/artists"
+          component={ArtistsSearchContainer}
+        />
+        <DefaultLayout exact path="/songs" component={TracksSearchContainer} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default Home;
